9f4db97e0a0bd95be14f3e4a5853b3fea9486c4b
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-21 21:22:05 +0700

    No.10

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,7 +6,7 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0,-10:F4}", 12.3456789);
+            Console.WriteLine("{0,-10:C}", 12.3456789);
         }
     }
 }
