9012a2cda92d7732b1bbed4cecc5582468e2cfe3
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-21 23:18:01 +0700

    uint

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,10 +6,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : ushort");
-            Console.WriteLine("Size :" + sizeof(ushort));
-            Console.WriteLine("Minimum Value :" + (int)ushort.MinValue);
-            Console.WriteLine("Maximum Value :" + (int)ushort.MaxValue);
+            Console.WriteLine("Data type : uint");
+            Console.WriteLine("Size :" + sizeof(uint));
+            Console.WriteLine("Minimum Value :" + uint.MinValue);
+            Console.WriteLine("Maximum Value :" + uint.MaxValue);
         }
     }
 }
