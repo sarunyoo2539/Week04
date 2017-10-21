8a3cda9829e020326831c33467a0add2ba37a533
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-21 23:23:25 +0700

    double

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,10 +6,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : float");
-            Console.WriteLine("Size :" + sizeof(float));
-            Console.WriteLine("Minimum Value :" + float.MinValue);
-            Console.WriteLine("Maximum Value :" + float.MaxValue);
+            Console.WriteLine("Data type : double");
+            Console.WriteLine("Size :" + sizeof(double));
+            Console.WriteLine("Minimum Value :" + double.MinValue);
+            Console.WriteLine("Maximum Value :" + double.MaxValue);
         }
     }
 }
