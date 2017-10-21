8139473cc25ae97ce2f4cf7f5ed0946c8c49e08f
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-21 21:26:01 +0700

    No.15

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,10 +6,7 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            int i;
-            Console.WriteLine("Value\tSquared\tCubed");
-            for (i = 1; i < 10; i++)
-                Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
+            Console.WriteLine("{0:#.###}.", 1234.56789);
 
         }
     }
