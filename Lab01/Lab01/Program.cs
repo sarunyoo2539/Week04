e1dcca85a3d4a97e5a7fb15e2b68545dc128f46f
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-21 23:11:54 +0700

    short

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,10 +6,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data type : sbyte");
-            Console.WriteLine("Size :" + sizeof(sbyte));
-            Console.WriteLine("Minimum Value :" + (int)sbyte.MinValue);
-            Console.WriteLine("Maximum Value :" + (int)sbyte.MaxValue);
+            Console.WriteLine("Data type : short");
+            Console.WriteLine("Size :" + sizeof(short));
+            Console.WriteLine("Minimum Value :" + (int)short.MinValue);
+            Console.WriteLine("Maximum Value :" + (int)short.MaxValue);
         }
     }
 }
