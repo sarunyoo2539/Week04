c0d6e8d9f8c92de06990b9451d33bb90f80bdee3
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-21 21:15:52 +0700

    No.7

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,7 +6,7 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("|{0,-10}|", 000);
+            Console.WriteLine("The value: {0}.", 500);
         }
     }
 }
