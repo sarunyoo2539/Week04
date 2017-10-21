721bd7db2f7ff3ad1b755808bc02f7362df9cf94
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-21 20:38:29 +0700

    Lab03P04

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,10 +6,7 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is text 1.");
-            Console.WriteLine("This is text 2.");
-            Console.WriteLine("This is text 3.");
-
+            Console.WriteLine(" {0} and {1}", 3, 6);
         }
     }
 }
