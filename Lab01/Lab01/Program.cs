a0a97e8f1291402c9bff11485d50233e11c72227
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-23 16:10:05 +0700

    or

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,17 +6,17 @@ namespace thruthTable
         static void Main(string[] args)
         {
             bool A, B, Y;
-            Console.WriteLine("      Y = A AND B");
+            Console.WriteLine("      Y = A OR B");
             Console.WriteLine("-----------------------");
             Console.WriteLine("   A      B\t|  Y");
             Console.WriteLine("-----------------------");
-            A = false; B = false; Y = A & B;
+            A = false; B = false; Y = A | B;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = false; B = true; Y = A & B;
+            A = false; B = true; Y = A | B;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = false; Y = A & B;
+            A = true; B = false; Y = A | B;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = true; Y = A & B;
+            A = true; B = true; Y = A | B;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
             Console.WriteLine("-----------------------");
         }
