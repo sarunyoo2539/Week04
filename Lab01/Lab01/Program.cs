b6ac004f377d7859f18a7f1f66674165221405d6
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-23 17:05:26 +0700

    Jupiter

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -9,11 +9,11 @@ namespace variableProperties
             Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
             const double mileTokm = 1.609344;
             Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
-            const double SunToMarsDistance = 227940000;  // miles
-            Console.WriteLine("SunToMarsDistance = {0} km", SunToMarsDistance * mileTokm);
-            double SunToMarsTimeOfLight = SunToMarsDistance / lightSpeed;  // miles
-            Console.WriteLine("SunToMarsTimeOfLight = {0} seconds", SunToMarsTimeOfLight);
-            Console.WriteLine("SunToMarsTimeOfLight = {0} minutes", SunToMarsTimeOfLight / 60d);
+            const double SunToJupiterDistance = 778330000;  // miles
+            Console.WriteLine("SunToJupiterDistance = {0} km", SunToJupiterDistance * mileTokm);
+            double SunToJupiterTimeOfLight = SunToJupiterDistance / lightSpeed;  // miles
+            Console.WriteLine("SunToJupiterTimeOfLight = {0} seconds", SunToJupiterTimeOfLight);
+            Console.WriteLine("SunToJupiterTimeOfLight = {0} minutes", SunToJupiterTimeOfLight / 60d);
             
         }
     }
