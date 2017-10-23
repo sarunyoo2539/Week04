24d0c490abc05d62aa1602eede7c2dcb3c91cc80
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-23 13:15:51 +0700

    Lab03P15-16

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-namespace RandomNumber
+namespace thruthTable
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int A = 0;
-            Random random = new Random();
-            int randomNumber = random.Next(0, 9);
-            bool B = A < randomNumber;
-            Console.WriteLine("Answer A < randomNumber = {0}" , B);
+            bool A, B, Y;
+            Console.WriteLine("      Y = A AND B");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("   A      B\t|  Y");
+            Console.WriteLine("-----------------------");
+            A = false; B = false; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = false; B = true; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = true; B = false; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = true; B = true; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            Console.WriteLine("-----------------------");
         }
     }
 }
