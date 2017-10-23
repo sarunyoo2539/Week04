7504b32d35bc90bb4bd31661cf2e5b2869e972d3
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-23 17:01:48 +0700

    Earth

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -9,11 +9,11 @@ namespace variableProperties
             Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
             const double mileTokm = 1.609344;
             Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
-            const double SunToVenusDistance = 108200000;  // miles
-            Console.WriteLine("SunToVenusDistance = {0} km", SunToVenusDistance * mileTokm);
-            double SunToEarthTimeOfLight = SunToVenusDistance / lightSpeed;  // miles
-            Console.WriteLine("SunToVenusTimeOfLight = {0} seconds", SunToEarthTimeOfLight);
-            Console.WriteLine("SunToVenusTimeOfLight = {0} minutes", SunToEarthTimeOfLight / 60d);
+            const double SunToEarthDistance = 149600000;  // miles
+            Console.WriteLine("SunToEarthDistance = {0} km", SunToEarthDistance * mileTokm);
+            double SunToEarthTimeOfLight = SunToEarthDistance / lightSpeed;  // miles
+            Console.WriteLine("SunToEarthTimeOfLight = {0} seconds", SunToEarthTimeOfLight);
+            Console.WriteLine("SunToEarthTimeOfLight = {0} minutes", SunToEarthTimeOfLight / 60d);
             
         }
     }
