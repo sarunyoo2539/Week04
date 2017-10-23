b17e0d629758286e74566a45efec5c16cc562b75
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-23 17:10:53 +0700

    Pluto

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -9,11 +9,11 @@ namespace variableProperties
             Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
             const double mileTokm = 1.609344;
             Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
-            const double SunToNeptuneDistance = 4501000000;  // miles
-            Console.WriteLine("SunToNeptuneDistance = {0} km", SunToNeptuneDistance * mileTokm);
-            double SunToNeptuneTimeOfLight = SunToNeptuneDistance / lightSpeed;  // miles
-            Console.WriteLine("SunToNeptuneTimeOfLight = {0} seconds", SunToNeptuneTimeOfLight);
-            Console.WriteLine("SunToNeptuneTimeOfLight = {0} minutes", SunToNeptuneTimeOfLight / 60d);
+            const double SunToPlutoDistance = 5945900000;  // miles
+            Console.WriteLine("SunToPlutoDistance = {0} km", SunToPlutoDistance * mileTokm);
+            double SunToPlutoTimeOfLight = SunToPlutoDistance / lightSpeed;  // miles
+            Console.WriteLine("SunToPlutoTimeOfLight = {0} seconds", SunToPlutoTimeOfLight);
+            Console.WriteLine("SunToPlutoTimeOfLight = {0} minutes", SunToPlutoTimeOfLight / 60d);
             
         }
     }
