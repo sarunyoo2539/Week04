db73dd6bb0e91df4b715330bd8d76b9a74973921
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-23 16:20:33 +0700

    not

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,19 +5,16 @@ namespace thruthTable
     {
         static void Main(string[] args)
         {
-            bool A, B, Y;
-            Console.WriteLine("      Y = A XOR B");
+            bool A, Y;
+            Console.WriteLine("      Y = NOT A");
             Console.WriteLine("-----------------------");
-            Console.WriteLine("   A      B\t|  Y");
+            Console.WriteLine("   A\t|  Y");
             Console.WriteLine("-----------------------");
-            A = false; B = false; Y = A ^ B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = false; B = true; Y = A ^ B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = false; Y = A ^ B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = true; Y = A ^ B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = false; Y = !A;
+            Console.WriteLine(" {0}\t| {1}", A, Y);
+            A = true; Y = !A;
+            Console.WriteLine(" {0}\t| {1}", A, Y);
+            
             Console.WriteLine("-----------------------");
         }
     }
